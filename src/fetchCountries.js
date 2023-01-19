deb0137f4afe18e5f0afae26bcd7bd731a61b507
HelenaUa <delena.1408@gmail.com>
2023-01-19 17:08:19 +0200

    add async

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,4 +1,4 @@
-// fetchCountries.js
+fetchCountries.js
 function fetchCountries(name) {
     const BASE_URL = "https://restcountries.com/v3.1";
     const properties = ["name", "capital", "population", "flags", "languages"];
@@ -10,5 +10,23 @@ function fetchCountries(name) {
     return response.json()
     })
 };
+
+
+// async function fetchCountries(name) {
+//     const BASE_URL = "https://restcountries.com/v3.1";
+//   const properties = ["name", "capital", "population", "flags", "languages"];
+
+//   try {
+// const response = await fetch(`${BASE_URL}/name/${name}?fields=${properties.join(',')}`);
+//    if (!response.ok) {
+//         throw new Error(response.statusText)
+//   };
+//   return await response.json();
+//   } catch (error) {
+//     console.log(error);
+//   };
+
+// };
+
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -89,3 +89,5 @@ Notiflix.Notify.init({
 });
 
 
+
+
